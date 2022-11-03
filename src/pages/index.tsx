23d9c6b255067e569a73e2fdf0afdde2419fff91
Aimen Sahnoun <aimensahnoun@outlook.com>
2022-11-03 14:43:06 +0300

    Initial project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>AimBridge</title>
+      </Head>
+    </div>
+  )
+}
