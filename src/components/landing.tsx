dd6bd9d1ac306f37c976eac5879b046788d3d026
Aimen Sahnoun <aimensahnoun@outlook.com>
2022-11-09 20:16:10 +0300

    responsive landing page

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image"
 
 // Dependencies import
-import {useConnectModal} from "@rainbow-me/rainbowkit"
+import { useConnectModal } from "@rainbow-me/rainbowkit"
 
 // Assets import
 import Exchange from "../../public/global.svg"
@@ -27,7 +27,7 @@ const LandingPage = () => {
         }
     ]
 
-    return <div className="flex items-center h-full w-[90%] justify-between">
+    return <div className="flex flex-col lg:flex-row items-center h-full w-[90%] justify-between">
         <div className="flex flex-col gap-y-2 items-center justify-center h-full">
             <span className="font-bold text-[5rem] magic " >Aim Bridge</span>
             <span className="font-semibold text-lg">A way to move your tokens between EVM networks.</span>
